f904c49f638816fdb6e415fc995f85eddcd2d68a
Dimitri Tarnavski <dimitri.tarn@gmail.com>
2017-12-31 15:18:29 +0100

    GUI Element "Label" hinzugefügt.

---
+++ gui/Label.cpp
@@ -0,0 +1,26 @@
+#include "Label.hpp"
+#include "../system/ResourceHolder.hpp"
+#include <SFML/Graphics/RenderTarget.hpp>
+
+gui::Label::Label(const std::string &text, const FontHolder &fonts)
+  : text(text, fonts.get(Fonts::ARCADE), 16)
+{
+
+}
+
+bool gui::Label::selectable() const {
+  return false;
+}
+
+void gui::Label::setText(const std::string &text) {
+  this->text.setString(text);
+}
+
+void gui::Label::draw(sf::RenderTarget &target, sf::RenderStates states) const {
+  states.transform *= getTransform();
+  target.draw(text, states);
+}
+
+void gui::Label::handleEvent(const sf::Event &event) {
+
+}
---
+++ gui/Label.hpp
@@ -0,0 +1,28 @@
+#ifndef BREAK_IT_LABEL_HPP
+#define BREAK_IT_LABEL_HPP
+
+#include <SFML/Graphics/Text.hpp>
+#include "Component.hpp"
+#include "../ResourceIdentifiers.hpp"
+
+namespace gui {
+  class Label : public Component{
+    public:
+      typedef std::shared_ptr<Label> Ptr;
+
+      Label(const std::string& text, const FontHolder& fonts);
+
+      bool selectable() const override;
+      void setText(const std::string& text);
+      void handleEvent(const sf::Event& event) override;
+
+    protected:
+      void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
+
+    private:
+      sf::Text text;
+  };
+}
+
+
+#endif //BREAK_IT_LABEL_HPP
