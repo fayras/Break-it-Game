d31baf8cd3afa896e69e39ede5c16b936b4b7c3a
Dimitri Tarnavski <dimitri.tarn@gmail.com>
2018-01-01 16:09:31 +0100

    Label werden nun aktualisiert, wenn eine neue Taste zugewiesen wird.

--- states/SettingsState.cpp
+++ states/SettingsState.cpp
@@ -1,30 +1,29 @@
 #include "SettingsState.hpp"
+#include "../system/Utility.hpp"
 #include <SFML/Graphics/RenderWindow.hpp>
 
 SettingsState::SettingsState(StateStack &stack, State::Context context)
     : State(stack, context)
 {
-  auto buttonMoveLeft = std::make_shared<gui::Button>(context);
-  buttonMoveLeft->setPosition(80.0f, 300.0f);
-  buttonMoveLeft->setText("Links");
-  buttonMoveLeft->setToggle(true);
-  gui::Button* buttonMoveLeftPointer = buttonMoveLeft.get();
+  addOption("Links", [context](const sf::Event& event, gui::Button* button, gui::Label* label) {
+    // sf::Keyboard::Key key = context.player->getAssignedKey(Player::MOVE_LEFT);
+    context.player->assignKey(Player::MOVE_LEFT, event.key.code);
+    button->deactivate();
+    label->setText(String::from(context.player->getAssignedKey(Player::MOVE_LEFT)));
+  });
 
-  options.insert(std::make_pair(
-    buttonMoveLeft,
-    [context, buttonMoveLeftPointer](const sf::Event& event) {
-      // sf::Keyboard::Key key = context.player->getAssignedKey(Player::MOVE_LEFT);
-      context.player->assignKey(Player::MOVE_LEFT, event.key.code);
-      buttonMoveLeftPointer->deactivate();
-    }
-  ));
+  addOption("Rechts", [context](const sf::Event& event, gui::Button* button, gui::Label* label) {
+    // sf::Keyboard::Key key = context.player->getAssignedKey(Player::MOVE_LEFT);
+    context.player->assignKey(Player::MOVE_RIGHT, event.key.code);
+    button->deactivate();
+    label->setText(String::from(context.player->getAssignedKey(Player::MOVE_RIGHT)));
+  });
 
   auto backButton = std::make_shared<gui::Button>(context);
   backButton->setPosition(80.0f, 600.0f);
   backButton->setText("Zurück");
   backButton->setCallback(std::bind(&SettingsState::requestStackPop, this));
 
-  guiContainer.pack(buttonMoveLeft);
   guiContainer.pack(backButton);
 }
 
@@ -43,14 +42,14 @@ bool SettingsState::handleEvent(const sf::Event &event) {
   bool isKeyBinding = false;
 
   for(const auto& option : options) {
-    if(option.first->active()) {
+    if(option.first.first->active()) {
       isKeyBinding = true;
       if(event.type == sf::Event::KeyReleased) {
         if(event.key.code == sf::Keyboard::Escape) {
-          option.first->deactivate();
+          option.first.first->deactivate();
           break;
         }
-        option.second(event);
+        option.second(event, option.first.first.get(), option.first.second.get());
       }
       break;
     }
@@ -63,4 +62,18 @@ bool SettingsState::handleEvent(const sf::Event &event) {
   }
 
   return false;
+}
+
+void SettingsState::addOption(const std::string &title, const std::function<void(const sf::Event&, gui::Button*, gui::Label*)>& func) {
+  auto button = std::make_shared<gui::Button>(context);
+  button->setPosition(80.0f, options.size() * 60 + 300);
+  button->setText(title);
+  button->setToggle(true);
+
+  auto label = std::make_shared<gui::Label>("", *getContext().fonts);
+  label->setPosition(300.0f, options.size() * 60 + 300);
+
+  options.insert(std::make_pair(std::make_pair(button, label), func));
+  guiContainer.pack(button);
+  guiContainer.pack(label);
 }
--- states/SettingsState.hpp
+++ states/SettingsState.hpp
@@ -19,8 +19,15 @@ class SettingsState : public State {
   private:
     sf::Text pausedText;
     gui::Container guiContainer;
-    std::map<gui::Button::Ptr, std::function<void(const sf::Event&)>> options;
-    std::vector<gui::Label::Ptr> bindingLabels;
+    std::map<
+        std::pair<gui::Button::Ptr, gui::Label::Ptr>,
+        std::function<void(const sf::Event&, gui::Button*, gui::Label*)>
+    > options;
+
+    void addOption(
+        const std::string &label,
+        const std::function<void(const sf::Event&, gui::Button*, gui::Label*)>& func
+    );
 };
 
 #endif //BREAK_IT_SETTINGSSTATE_HPP
