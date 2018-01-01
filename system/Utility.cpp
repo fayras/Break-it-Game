817474f390297df56ecdece93c521658426b9e96
Dimitri Tarnavski <dimitri.tarn@gmail.com>
2018-01-01 15:59:35 +0100

    Hilfsfunktion zum Umwandeln von Keyboard::Key zu String hinzugefügt.

--- system/Utility.cpp
+++ system/Utility.cpp
@@ -1,12 +1,8 @@
 #include "Utility.hpp"
 #include "Animation.hpp"
 
-#include <SFML/Graphics/Sprite.hpp>
 #include <SFML/Graphics/Text.hpp>
-
 #include <random>
-#include <cmath>
-#include <ctime>
 #include <cassert>
 
 namespace {
@@ -62,4 +58,115 @@ float Vector::length(sf::Vector2f vector) {
 sf::Vector2f Vector::unit(sf::Vector2f vector) {
   assert(vector != sf::Vector2f(0.f, 0.f));
   return vector / length(vector);
+}
+
+std::string String::from(sf::Keyboard::Key key) {
+#define SFML_KEY_TO_STRING(KEY) case sf::Keyboard::KEY: return #KEY;
+
+  switch (key) {
+    SFML_KEY_TO_STRING(Unknown)
+    SFML_KEY_TO_STRING(A)
+    SFML_KEY_TO_STRING(B)
+    SFML_KEY_TO_STRING(C)
+    SFML_KEY_TO_STRING(D)
+    SFML_KEY_TO_STRING(E)
+    SFML_KEY_TO_STRING(F)
+    SFML_KEY_TO_STRING(G)
+    SFML_KEY_TO_STRING(H)
+    SFML_KEY_TO_STRING(I)
+    SFML_KEY_TO_STRING(J)
+    SFML_KEY_TO_STRING(K)
+    SFML_KEY_TO_STRING(L)
+    SFML_KEY_TO_STRING(M)
+    SFML_KEY_TO_STRING(N)
+    SFML_KEY_TO_STRING(O)
+    SFML_KEY_TO_STRING(P)
+    SFML_KEY_TO_STRING(Q)
+    SFML_KEY_TO_STRING(R)
+    SFML_KEY_TO_STRING(S)
+    SFML_KEY_TO_STRING(T)
+    SFML_KEY_TO_STRING(U)
+    SFML_KEY_TO_STRING(V)
+    SFML_KEY_TO_STRING(W)
+    SFML_KEY_TO_STRING(X)
+    SFML_KEY_TO_STRING(Y)
+    SFML_KEY_TO_STRING(Z)
+    SFML_KEY_TO_STRING(Num0)
+    SFML_KEY_TO_STRING(Num1)
+    SFML_KEY_TO_STRING(Num2)
+    SFML_KEY_TO_STRING(Num3)
+    SFML_KEY_TO_STRING(Num4)
+    SFML_KEY_TO_STRING(Num5)
+    SFML_KEY_TO_STRING(Num6)
+    SFML_KEY_TO_STRING(Num7)
+    SFML_KEY_TO_STRING(Num8)
+    SFML_KEY_TO_STRING(Num9)
+    SFML_KEY_TO_STRING(Escape)
+    SFML_KEY_TO_STRING(LControl)
+    SFML_KEY_TO_STRING(LShift)
+    SFML_KEY_TO_STRING(LAlt)
+    SFML_KEY_TO_STRING(LSystem)
+    SFML_KEY_TO_STRING(RControl)
+    SFML_KEY_TO_STRING(RShift)
+    SFML_KEY_TO_STRING(RAlt)
+    SFML_KEY_TO_STRING(RSystem)
+    SFML_KEY_TO_STRING(Menu)
+    SFML_KEY_TO_STRING(LBracket)
+    SFML_KEY_TO_STRING(RBracket)
+    SFML_KEY_TO_STRING(SemiColon)
+    SFML_KEY_TO_STRING(Comma)
+    SFML_KEY_TO_STRING(Period)
+    SFML_KEY_TO_STRING(Quote)
+    SFML_KEY_TO_STRING(Slash)
+    SFML_KEY_TO_STRING(BackSlash)
+    SFML_KEY_TO_STRING(Tilde)
+    SFML_KEY_TO_STRING(Equal)
+    SFML_KEY_TO_STRING(Dash)
+    SFML_KEY_TO_STRING(Space)
+    SFML_KEY_TO_STRING(Return)
+    SFML_KEY_TO_STRING(BackSpace)
+    SFML_KEY_TO_STRING(Tab)
+    SFML_KEY_TO_STRING(PageUp)
+    SFML_KEY_TO_STRING(PageDown)
+    SFML_KEY_TO_STRING(End)
+    SFML_KEY_TO_STRING(Home)
+    SFML_KEY_TO_STRING(Insert)
+    SFML_KEY_TO_STRING(Delete)
+    SFML_KEY_TO_STRING(Add)
+    SFML_KEY_TO_STRING(Subtract)
+    SFML_KEY_TO_STRING(Multiply)
+    SFML_KEY_TO_STRING(Divide)
+    SFML_KEY_TO_STRING(Left)
+    SFML_KEY_TO_STRING(Right)
+    SFML_KEY_TO_STRING(Up)
+    SFML_KEY_TO_STRING(Down)
+    SFML_KEY_TO_STRING(Numpad0)
+    SFML_KEY_TO_STRING(Numpad1)
+    SFML_KEY_TO_STRING(Numpad2)
+    SFML_KEY_TO_STRING(Numpad3)
+    SFML_KEY_TO_STRING(Numpad4)
+    SFML_KEY_TO_STRING(Numpad5)
+    SFML_KEY_TO_STRING(Numpad6)
+    SFML_KEY_TO_STRING(Numpad7)
+    SFML_KEY_TO_STRING(Numpad8)
+    SFML_KEY_TO_STRING(Numpad9)
+    SFML_KEY_TO_STRING(F1)
+    SFML_KEY_TO_STRING(F2)
+    SFML_KEY_TO_STRING(F3)
+    SFML_KEY_TO_STRING(F4)
+    SFML_KEY_TO_STRING(F5)
+    SFML_KEY_TO_STRING(F6)
+    SFML_KEY_TO_STRING(F7)
+    SFML_KEY_TO_STRING(F8)
+    SFML_KEY_TO_STRING(F9)
+    SFML_KEY_TO_STRING(F10)
+    SFML_KEY_TO_STRING(F11)
+    SFML_KEY_TO_STRING(F12)
+    SFML_KEY_TO_STRING(F13)
+    SFML_KEY_TO_STRING(F14)
+    SFML_KEY_TO_STRING(F15)
+    SFML_KEY_TO_STRING(Pause)
+  }
+
+  return "";
 }
--- system/Utility.hpp
+++ system/Utility.hpp
@@ -1,7 +1,9 @@
 #ifndef SFML_TEMPLATE_UTILITY_HPP
 #define SFML_TEMPLATE_UTILITY_HPP
 
+#include <string>
 #include <SFML/System/Vector2.hpp>
+#include <SFML/Window/Keyboard.hpp>
 
 class Animation;
 namespace sf {
@@ -27,6 +29,10 @@ namespace Random {
 namespace Vector {
   float	length(sf::Vector2f vector);
   sf::Vector2f unit(sf::Vector2f vector);
+}
+
+namespace String {
+  std::string from(sf::Keyboard::Key key);
 }
 
 #endif //SFML_TEMPLATE_UTILITY_HPP
